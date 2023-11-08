c9cb0bf198fa9e2ea1742ae43d289eb0a9cab40a
prxs <prxs@pop-os.localdomain>
2023-11-08 16:02:32 -0300

    feat: message alert / message add cadastro

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
 
 def cadastro(request):
     if request.method == "GET":
@@ -11,14 +13,17 @@ def cadastro(request):
         confirma_senha = request.POST.get('confirm-password')
         
         if senha != confirma_senha:
+            messages.add_message(request, constants.ERROR, 'As senhas não coincidem')
             return redirect('/auth/cadastro/')
         
         if len(username.strip()) == 0 or len(senha.strip()) == 0:
+            messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
             return redirect('/auth/cadastro/')
         
         user = User.objects.filter(username=username)
         
         if user.exists():
+            messages.add_message(request, constants.ERROR, 'Já existe um usuário com esse username')
             return redirect('/auth/cadastro')
         
         try:
@@ -26,8 +31,10 @@ def cadastro(request):
                                             password=senha)
             
             user.save()
+            messages.add_message(request, constants.SUCCESS, 'Usuário criado com sucesso')
             return redirect('/auth/logar/')
         except Exception:
+            messages.add_message(request, constants.ERROR, 'Erro interno do sistema')
             return redirect('/auth/cadastro/')
 
 def logar(request):
