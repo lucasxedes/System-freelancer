7e6048280487b50129c4550e78f570320973ebd1
prxs <prxs@pop-os.localdomain>
2023-11-23 13:51:55 -0300

    :sparkles: feat: Jobs - views/perfil , enviar projeto

--- jobs/urls.py
+++ jobs/urls.py
@@ -6,4 +6,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('encotrar_jobs/', views.encontrar_jobs, name="encontrar_jobs"),
     path('aceitar_job/<int:id>/', views.aceitar_job, name="aceitar_job"),
+    path('perfil/', views.perfil, name="perfil"),
+    path('enviar_projeto/', views.enviar_projeto, name="enviar_projeto"),
 ]
